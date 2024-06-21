7bddbb37aa603c100ba9e433af5eec2b216b0a31
kekemotor <derebenko23@gmail.com>
2024-06-21 12:31:29 +0300

    New: Наконец-то header

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,12 +4,14 @@
       <q-header elevated class="bg-primary text-white">
           <q-toolbar>
               <q-toolbar-title class="flex items-center" :class="{'justify-between': $q.screen.lt.sm}">
-                  <img src="../assets/logo.png"/>
-                  
-                  <p v-if="!$q.screen.lt.sm">
-                    GOZON
-                  </p>
-                  <q-btn round dense flat icon="person_outline" class="text-white cursor-pointer" @click="goToProfile()"></q-btn>
+                    <img src="../assets/logo.png"/>
+
+                    <p v-if="!$q.screen.lt.sm">
+                      GOZON
+                    </p>
+                    <div @click="toUser" class=" cursor-pointer" style="flex: 1 1 0%; text-align: right;">
+                        <p style="display: inline;">Личный кабинет</p> <q-icon name="person_outline" class="text-white" size="30px"></q-icon>
+                    </div>
               </q-toolbar-title>
               
           </q-toolbar>
@@ -24,82 +26,14 @@
   </q-layout>
 </template>
 
-<script setup>
-import {ref, onMounted, watch} from 'vue'
-import {useRouter} from "vue-router";
-
-import { useQuasar } from 'quasar'
-const $q = useQuasar()
-
-const isMiniDrawer = ref($q.screen.lt.md);
-
-const isDrawerOpen = ref(true);
-const toggleLeftDrawer = () => {
-  
-  if ($q.screen.lt.sm) {
-      isDrawerOpen.value = false
-      return
-  }
-  
-  isMiniDrawer.value = !isMiniDrawer.value
-};
-
-const menuList = [
-{
-      icon: 'fa-solid fa-calendar-days',
-      label: 'Расписание',
-      to: '/schedule'
-  },
-  {
-      icon: 'done',
-      label: 'Посещения',
-      to: '/visits'
-  },
-  {
-      icon: 'fas fa-user',
-      label: 'Ученики',
-      to: '/students'
-  },
-  {
-      icon: 'send',
-      label: 'Педагоги',
-      to: '/teachers'
-  },
-  {
-      icon: 'business_center',
-      label: 'Занятия',
-      to: '/lessons'
-  },
-];
-
-const router = useRouter()
-
-watch(() => $q.screen.width, newValue => {
-  if ($q.screen.gt.xs) {
-      isDrawerOpen.value = true
-  }
-  else {
-      isMiniDrawer.value = true
-  }
-});
-
-onMounted(() => {
-  isDrawerOpen.value = $q.screen.gt.xs
-  try {
-      const routeName = localStorage.getItem('prevRoute')
-      console.log(routeName)
-      if (routeName) {
-          router.push({
-              name: routeName,
-          });
-      }
-  }
-  catch (e) {
-      router.push({
-          name: RouterPages.SCHEDULE,
-      });
-  }
-})
+<script>
+export default{
+    methods:{
+        toUser(){
+            window.location.href = "user"
+        }
+    }
+}
 
 </script>
 
---
+++ src/pages/login.vue
@@ -0,0 +1,9 @@
+<template>
+    <div style="display:flex; justify-content: center ">
+        <div style="display:flex; width: 100%; align-items: center ">
+            <h1>sd</h1>
+        </div>
+    </div>
+
+
+</template>
