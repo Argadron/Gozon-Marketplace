e866ace8874498495b74bcadca251d362c5c7913
Argadron <argadron@bk.ru>
2024-07-13 13:56:44 +0500

    Fix: Payment controller macro fix

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Delete, Get, ParseIntPipe, Param, UseGuards } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags, ApiBearerAuth } from '@nestjs/swagger';
 import { AdminGuard } from '@guards/admin.guard';
 import { SwaggerBadRequest, SwaggerForbiddenException, SwaggerNoContent, SwaggerNotFound, SwaggerOK, SwaggerUnauthorizedException } from '@swagger/apiResponse.interfaces';
 import { JwtGuard } from '@guards/jwt.guard';
@@ -16,6 +16,7 @@ export class PaymentsController {
   @ApiResponse({ status: 200, description: "Payments getted", type: SwaggerOK })
   @ApiResponse({ status: 401, description: "Token Invalid/Unauthorized", type: SwaggerUnauthorizedException })
   @ApiResponse({ status: 403, description: "Your role not have access to this action", type: SwaggerForbiddenException })
+  @ApiBearerAuth()
   async getAll() {
     return await this.paymentsService.getAll()
   }
@@ -27,6 +28,7 @@ export class PaymentsController {
   @ApiResponse({ status: 401, description: "Token Invalid/Unauthorized", type: SwaggerUnauthorizedException })
   @ApiResponse({ status: 403, description: "Your role not have access to this action", type: SwaggerForbiddenException })
   @ApiResponse({ status: 404, description: "Payment not found", type: SwaggerNotFound })
+  @ApiBearerAuth()
   async delete(@Param("id", ParseIntPipe) id: number) {
     return await this.paymentsService.delete(id)
   }
